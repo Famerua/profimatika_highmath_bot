9ec30595c766b3f4db1eca042c1adb7ff930c512
Taubay <taubay.kozh@gmail.com>
2024-07-13 15:25:17 +0600

    added /stats

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ from aiogram.filters import Command, StateFilter
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import default_state, State, StatesGroup
 from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram.types import Message, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove, InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.types import (
+    Message,
+    KeyboardButton,
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+)
 from csv import DictReader, DictWriter, writer
 import aiogram.exceptions
 import datetime
@@ -17,6 +24,7 @@ dp = Dispatcher(storage=storage)
 
 NOTIFICATION_MESSAGE: Message
 
+
 class FSMwaitnotification(StatesGroup):
     notification_message = State()
     add_ref_question = State()
@@ -27,19 +35,20 @@ class FSMwaitnotification(StatesGroup):
 def is_admin(message: Message):
     return message.from_user.id in [5552984613, 760337093]
 
-@dp.message(Command(commands='cancel'), ~StateFilter(default_state))
+
+@dp.message(Command(commands="cancel"), ~StateFilter(default_state))
 async def process_cancel_command_state(message: Message, state: FSMContext):
     if is_admin(message):
         await message.answer(
-            text='Рассылка отменена',
-            reply_markup=ReplyKeyboardRemove()
+            text="Рассылка отменена", reply_markup=ReplyKeyboardRemove()
         )
         # Сбрасываем состояние и очищаем данные, полученные внутри состояний
         await state.clear()
     else:
         await message.answer("Вы кто такой? Я вас не знаю!")
 
-#admin
+
+# admin
 @dp.message(Command(commands=["notificate"]), StateFilter(default_state))
 async def admin_notification(message: Message, state: FSMContext):
     if is_admin(message):
@@ -49,19 +58,21 @@ async def admin_notification(message: Message, state: FSMContext):
         await message.answer("Вы кто такой? Я вас не знаю!")
 
 
-#admin
+# admin
 @dp.message(StateFilter(FSMwaitnotification.add_ref_question))
 async def ask_about_ref(message: Message, state: FSMContext):
-    print('Бот в состоянии (Получил сообщение. Задал вопрос о ссылке)')
+    print("Бот в состоянии (Получил сообщение. Задал вопрос о ссылке)")
 
     global NOTIFICATION_MESSAGE
     NOTIFICATION_MESSAGE = message
 
-    button_add_ref_yes = KeyboardButton(text='Да')
-    button_add_ref_no = KeyboardButton(text='Нет')
-    keyboard_add_ref = ReplyKeyboardMarkup(keyboard=[[button_add_ref_yes, button_add_ref_no]], resize_keyboard=True)
+    button_add_ref_yes = KeyboardButton(text="Да")
+    button_add_ref_no = KeyboardButton(text="Нет")
+    keyboard_add_ref = ReplyKeyboardMarkup(
+        keyboard=[[button_add_ref_yes, button_add_ref_no]], resize_keyboard=True
+    )
 
-    await message.answer('Добавить ссылку?', reply_markup=keyboard_add_ref)
+    await message.answer("Добавить ссылку?", reply_markup=keyboard_add_ref)
     await state.set_state(FSMwaitnotification.take_ans_ref_question)
 
 
@@ -70,64 +81,92 @@ async def sending_message(message: Message, keyboard=None):
     try:
         with open("users.csv", encoding="utf-8") as file:
             users = DictReader(file)
-            with open("sent_messages.csv", "a", newline='', encoding="utf-8") as sent_file:
+            with open(
+                "sent_messages.csv", "a", newline="", encoding="utf-8"
+            ) as sent_file:
                 fieldnames = ["chat_id", "message_id", "timestamp"]
                 writer = DictWriter(sent_file, fieldnames=fieldnames)
                 for user in users:
                     try:
                         if keyboard is None:
-                            sent_message = await message.send_copy(chat_id=user["chat_id"])
+                            sent_message = await message.send_copy(
+                                chat_id=user["chat_id"]
+                            )
                         else:
-                            sent_message = await message.send_copy(chat_id=user["chat_id"], reply_markup=keyboard)
-                        
+                            sent_message = await message.send_copy(
+                                chat_id=user["chat_id"], reply_markup=keyboard
+                            )
+
                         # Сохраняем chat_id, message_id и время отправки
-                        writer.writerow({
-                            "chat_id": user["chat_id"],
-                            "message_id": sent_message.message_id,
-                            "timestamp": datetime.datetime.now().isoformat()
-                        })   
+                        writer.writerow(
+                            {
+                                "chat_id": user["chat_id"],
+                                "message_id": sent_message.message_id,
+                                "timestamp": datetime.datetime.now().isoformat(),
+                            }
+                        )
                     except aiogram.exceptions.TelegramBadRequest as e:
                         # Логируем ошибку и продолжаем с другим пользователем
-                        print(f"Ошибка при отправке сообщения в чат {user['chat_id']}: {e}")
+                        print(
+                            f"Ошибка при отправке сообщения в чат {user['chat_id']}, @{user['username']}: {e}"
+                        )
                     except aiogram.exceptions.TelegramForbiddenError as e:
                         # Логируем ошибку и продолжаем с другим пользователем
                         print(f"Нет доступа к чату {user['chat_id']}: {e}")
                     except Exception as e:
-                    # Логируем любую другую ошибку и продолжаем с другим пользователем
-                        print(f"Неизвестная ошибка при отправке сообщения в чат {user['chat_id']}: {e}")
-                print('Бот разослал')
+                        # Логируем любую другую ошибку и продолжаем с другим пользователем
+                        print(
+                            f"Неизвестная ошибка при отправке сообщения в чат {user['chat_id']}, @{user['username']}: {e}"
+                        )
+                print("Бот разослал")
     except TypeError:
         await message.reply(text="Не могу это переотправить")
     except Exception as e:
         await message.reply(text=f"Что-то пошло не так: {e}")
 
-#admin
+
+# admin
 @dp.message(StateFilter(FSMwaitnotification.take_ans_ref_question))
 async def take_ans_ref_question(message: Message, state: FSMContext):
-    if message.text == 'Да':
-        print('Бот в состоянии (Получаю ссылку)')
-        await message.answer("Жду от вас название ссылки и саму ссылку в таком формате", reply_markup=ReplyKeyboardRemove())
+    if message.text == "Да":
+        print("Бот в состоянии (Получаю ссылку)")
+        await message.answer(
+            "Жду от вас название ссылки и саму ссылку в таком формате",
+            reply_markup=ReplyKeyboardRemove(),
+        )
         await message.answer("Название ссылки\n\nСсылка")
         await state.set_state(FSMwaitnotification.wait_ref)
-    elif message.text == 'Нет':
-        await message.answer('Хорошо. Рассылаю сообщение!', reply_markup=ReplyKeyboardRemove())
+    elif message.text == "Нет":
+        await message.answer(
+            "Хорошо. Рассылаю сообщение!", reply_markup=ReplyKeyboardRemove()
+        )
         # await state.set_state(FSMwaitnotification.ask_about_button)
         await sending_message(NOTIFICATION_MESSAGE)
         await state.clear()
 
+
 @dp.message(StateFilter(FSMwaitnotification.wait_ref))
 async def take_ref(message: Message, state: FSMContext):
-    url = message.text.split('\n\n')
-    if  len(url) == 2:
-        print('Бот в состояннии (Получил ссылку)')
+    url = message.text.split("\n\n")
+    if len(url) == 2:
+        print("Бот в состояннии (Получил ссылку)")
         url_inline_button = InlineKeyboardButton(text=url[0], url=url[1])
         keyboard_ref = InlineKeyboardMarkup(inline_keyboard=[[url_inline_button]])
-        await message.answer('Принял! Начинаю рассылку с ссылкой')
+        await message.answer("Принял! Начинаю рассылку с ссылкой")
         await sending_message(NOTIFICATION_MESSAGE, keyboard_ref)
         await state.clear()
     else:
-        await message.answer('Некорректный формат. Введите /cancel, чтобы начать рассылку заново')
+        await message.answer(
+            "Некорректный формат. Введите /cancel, чтобы начать рассылку заново"
+        )
 
+
+@dp.message(Command(commands=["stats"]), is_admin, StateFilter(default_state))
+async def get_stats(message: Message):
+    with open("users.csv") as file:
+        Users = DictReader(file)
+        readers = [f"@{user['username']}" for user in Users]
+    await message.answer(f"Активные читатели {len(readers)}:\n{"\n".join(readers)}")
 
 
 @dp.message(Command(commands=["start"]), StateFilter(default_state))
@@ -138,7 +177,7 @@ async def process_start_command(message: Message):
 <em>Данный бот будет уведомлять тебя о выходе новых видео на канале Макса, также ты одним из первых будешь получать актуальную информацию об онлайн трансляциях и вебинарах с курсов по <b>Высшей Математике</b> и узнавать о самых приятных ценах на грядущие курсы)</em>
 
 <b>Если ты согласен, то напиши команду /activate</b>""",
-        parse_mode="HTML"
+        parse_mode="HTML",
     )
 
 
@@ -153,9 +192,9 @@ async def process_help_command(message: Message):
 async def process_activate_command(message: Message):
     user_id = str(message.from_user.id)  # Преобразуем user_id в строку
     is_subscribed = False
-    
+
     # Читаем CSV файл и проверяем наличие user_id
-    with open("users.csv", newline='') as file:
+    with open("users.csv", newline="") as file:
         Reader = DictReader(file)
         for row in Reader:
             if row["user_id"] == user_id:
@@ -166,29 +205,35 @@ async def process_activate_command(message: Message):
         await message.answer("Вы уже подписаны!")
     else:
         # Добавляем новую запись в CSV файл
-        with open("users.csv", 'a', newline='') as csvfile:
+        with open("users.csv", "a", newline="") as csvfile:
             Writer = writer(csvfile)
-            Writer.writerow([message.from_user.id, message.chat.id])
+            Writer.writerow(
+                [
+                    message.from_user.id,
+                    message.chat.id,
+                    message.from_user.username,
+                ]
+            )
         await message.answer("Вы подписались на рассылку!")
 
 
 @dp.message(Command(commands=["deactivate"]), StateFilter(default_state))
 async def process_deactivate_command(message: Message):
     user_id = str(message.from_user.id)  # Преобразуем user_id в строку
-    
+
     # Проверяем, подписан ли пользователь
-    with open("users.csv", newline='') as file:
+    with open("users.csv", newline="") as file:
         if user_id not in {row["user_id"] for row in DictReader(file)}:
             await message.answer("Вы и не подписаны...")
             return
-    
+
     # Читаем CSV файл и удаляем пользователя
-    with open("users.csv", 'r', newline='') as csvfile:
+    with open("users.csv", "r", newline="") as csvfile:
         reader = DictReader(csvfile)
-        rows = [row for row in reader if row['user_id'] != user_id]
+        rows = [row for row in reader if row["user_id"] != user_id]
 
     # Записываем обновленный список пользователей обратно в CSV файл
-    with open("users.csv", 'w', newline='') as csvfile:
+    with open("users.csv", "w", newline="") as csvfile:
         fieldnames = reader.fieldnames
         writer = DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
